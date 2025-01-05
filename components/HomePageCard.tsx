ff8d43282dfd56100e1564bf1597a5ce480b958f
Welch <weq511762338@gmail.com>
2025-01-05 16:40:57 -0500

    fixed project page link over expanded issue

--- components/HomePageCard.tsx
+++ components/HomePageCard.tsx
@@ -13,26 +13,24 @@ interface HomePageCardProps {
 function HomePageCard({ org }: HomePageCardProps) {
     const [data] = useDocumentData(doc(db, "organizations", org.orgId));
     const basePath = `/org/${org.orgId}`;
-
     return (
-        <Link href={basePath}>
-            <div className="group-card flex flex-col shadow-md mb-4 rounded-xl overflow-hidden bg-white dark:bg-gray-800 w-64 h-64 md:w-96 md:h-96 hover:translate-y-[-4px] transition-transform duration-300">
-                <div className="bg-gradient-to-r from-indigo-500 to-purple-500 p-4 text-white flex flex-col gap-2">
-                    <h1 className="text-2xl font-semibold">
-                        {data?.title}
-                    </h1>
-
-                    <div className='gap-1'>
-                        {/* <p className="text-sm">{org.orgId}</p> */}
-                        <h3 className="dark:text-gray-100 text-sm">
-                            Admin: {data?.admins[0]}
-                        </h3>
+        <Link href={basePath} className="block w-96 h-96">
+            <div className="flex flex-col shadow-md mb-4 rounded-xl overflow-hidden bg-white dark:bg-gray-800 hover:translate-y-[-4px] transition-transform duration-300 w-96 h-96">
+                <div className="w-full h-full">
+                    <div className="bg-gradient-to-r from-indigo-500 to-purple-500 p-4 text-white flex flex-col gap-2">
+                        <h1 className="text-2xl font-semibold">
+                            {data?.title}
+                        </h1>
+                        <div className='gap-1'>
+                            {/* <p className="text-sm">{org.orgId}</p> */}
+                            <h3 className="dark:text-gray-100 text-sm">
+                                Admin: {data?.admins[0]}
+                            </h3>
+                        </div>
                     </div>
-
-                </div>
-                <div className="flex-grow bg-white dark:bg-gray-800 p-4">
-
-                    <h3>Description: {data?.description} </h3>
+                    <div className="flex-grow bg-white dark:bg-gray-800 p-4">
+                        <h3>Description: {data?.description} </h3>
+                    </div>
                 </div>
             </div>
         </Link>

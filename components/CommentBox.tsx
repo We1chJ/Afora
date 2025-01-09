873648e599e5afa08eea17947f08d2c84bde8a65
Welch <weq511762338@gmail.com>
2025-01-08 22:02:14 -0500

    temp

--- components/CommentBox.tsx
+++ components/CommentBox.tsx
@@ -1,34 +1,28 @@
-'use client'
+'use client';
 
-import { useState, useRef, useEffect } from 'react'
-import { CircleUser, SendHorizontal } from 'lucide-react'
-import { Button } from "@/components/ui/button"
-import dynamic from 'next/dynamic'
-import React from 'react'
-import { MDXEditorMethods } from '@mdxeditor/editor'
-
+import { useState } from 'react';
+import { CircleUser, SendHorizontal } from 'lucide-react';
+import { Button } from "@/components/ui/button";
+import React from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 function CommentBox() {
-  const [comment, setComment] = useState('')
-  const ref = React.useRef<MDXEditorMethods>(null)
+  const [comment, setComment] = useState('');
 
   const handlePost = () => {
-    console.log('Posted comment:', comment)
-    setComment('')
-  }
+    console.log('Posted comment:', comment);
+    setComment('');
+  };
 
-  // suggested to set to false but true seems fine
-  const EditorComp = dynamic(() => import('./MDTextEditor'), { ssr: true })
   return (
     <div className="flex items-center w-full space-x-2 p-3 bg-white rounded-lg shadow">
       <CircleUser className="w-10 h-10 text-gray-400" />
       <div className="h-full w-full">
-        <div className="flex mb-2">
-
-        </div>
+        <div className="flex mb-2"></div>
         <div className="w-full rounded-md overflow-hidden flex items-end">
-          <div className="w-full flex flex-col">
-            <EditorComp editorRef={ref} markdown={comment} />
+          <div className="w-full flex flex-col h-fit">
+            <ReactQuill theme="snow" value={comment} onChange={setComment} className="w-full h-full" />
           </div>
           <Button
             variant="default"
@@ -41,7 +35,7 @@ function CommentBox() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default CommentBox
+export default CommentBox;

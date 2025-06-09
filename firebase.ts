f6ef0470635bccb1313ddb2cdcb84f136c6144b9
Thresh514 <165234885+Thresh514@users.noreply.github.com>
2025-06-09 13:45:38 +0800

    add new database

--- firebase.ts
+++ firebase.ts
@@ -9,19 +9,19 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDdffASPvRB5EH9Y4J7-CIsjFc9F_2w1pI",
-  authDomain: "notion-clone-2aa24.firebaseapp.com",
-  projectId: "notion-clone-2aa24",
-  storageBucket: "notion-clone-2aa24.firebasestorage.app",
-  messagingSenderId: "123461252894",
-  appId: "1:123461252894:web:34bc3dfdb0f3c03134bb0f",
-  measurementId: "G-ZBHREJ1F4Y"
+  apiKey: "AIzaSyAeM_jITuesHwfKKFX4Pqh9aC2srA1fZfs",
+  authDomain: "afora-42edb.firebaseapp.com",
+  projectId: "afora-42edb",
+  storageBucket: "afora-42edb.firebasestorage.app",
+  messagingSenderId: "521371419871",
+  appId: "1:521371419871:web:1b7ccaebd59ed944bad56a",
+  measurementId: "G-V59K1CG4ZE"
 };
 
 const app = getApps().length === 0? initializeApp(firebaseConfig) : getApp();
 // NOTE: this db is for client side and all normal operations such as data fetching
 const db = getFirestore(app);
 const storage = getStorage();
-const storageBucket = 'notion-clone-2aa24.firebasestorage.app';
+const storageBucket = 'afora-42edb.firebasestorage.app';
 
 export { db, app, storage, storageBucket }

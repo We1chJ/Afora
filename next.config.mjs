2f85441d6e3ab1bc44eb3079780beb3dd075164c
Thresh514 <165234885+Thresh514@users.noreply.github.com>
2025-07-27 21:19:39 -0400

    fix env

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['img.clerk.com', 'images.pexels.com', 'pexels.com'],
+        domains: ['img.clerk.com', 'images.pexels.com', 'pexels.com', 'static.vecteezy.com'],
     },
 };
 // next.config.js
